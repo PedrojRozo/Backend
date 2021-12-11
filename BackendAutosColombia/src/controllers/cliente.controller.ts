1d44d35757a4da2ee11e904d6a4f6209f8ca9dd2
PedroJRozo <pedrojrozo@gmail.com>
2021-12-11 12:52:52 -0500

    prueba10

--- BackendAutosColombia/src/controllers/cliente.controller.ts
+++ BackendAutosColombia/src/controllers/cliente.controller.ts
@@ -211,3 +211,27 @@ export class ClienteController {
     await this.clienteRepository.deleteById(id);
   }
 }
+
+
+SwaggerUI({
+  //  ...other configuration options,
+  plugins: [function UrlParamDisablePlugin() {
+    return {
+      statePlugins: {
+        spec: {
+          wrapActions: {
+            // Remove the ?url parameter from loading an external OpenAPI definition.
+            updateUrl: (oriAction) => (payload) => {
+              const url = new URL(window.location.href)
+              if (url.searchParams.has('url')) {
+                url.searchParams.delete('url')
+                window.location.replace(url.toString())
+              }
+              return oriAction(payload)
+            }
+          }
+        }
+      }
+    }
+  }],
+})
